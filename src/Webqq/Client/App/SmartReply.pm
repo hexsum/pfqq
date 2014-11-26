a9dd272c1d8fbb57fcf6ce7dec783adbfb36a3f7
sjdy521 <sjdy521@163.com>
2014-11-26 13:14:14 +0800

    4）Webqq::Client::App::SmartReply回复消息时会按照80%的概率先@对方 	修改：      Changes 	修改：      src/Webqq/Client/App/SmartReply.pm

--- src/Webqq/Client/App/SmartReply.pm
+++ src/Webqq/Client/App/SmartReply.pm
@@ -2,6 +2,7 @@ package Webqq::Client::App::SmartReply;
 use Exporter 'import';
 use JSON;
 use Encode;
+use Webqq::Client::Util qw(truncate);
 @EXPORT=qw(SmartReply);
 my $API = 'http://www.tuling123.com/openapi/api';
 #my $API = 'http://www.xiaodoubi.com/bot/api.php?chat=';
@@ -11,6 +12,7 @@ sub SmartReply{
     return unless $msg->{content} =~/\@小灰 /;
     my $input = $msg->{content};
     my $userid = $msg->from_qq;
+    my $from_nick = $msg->from_nick;
     $input=~s/\@[^ ]+ |\[[^\[\]]+\]\x01|\[[^\[\]]+\]//g;
     my @query_string = (
         "key"       =>  "4c53b48522ac4efdfe5dfb4f6149ae51",
@@ -19,16 +21,12 @@ sub SmartReply{
     );
     my @query_string_pairs;
     push @query_string_pairs , shift(@query_string) . "=" . shift(@query_string) while(@query_string);
-    my $res;
-    eval{
-        local $SIG{ALRM} = sub{die "timout\n"};
-        alarm 5;
-        $res = $client->{ua}->get($API . "?" . join("&",@query_string_pairs));
-        alarm 0;
-    };
-    print "Webqq::Client::App::SmartReply请求超时\n" if $@ and $client->{debug}; 
-    if($res->is_success){
-        print $res->content,"\n" if $client->{debug};
+    $client->{asyn_ua}->get($API . "?" . join("&",@query_string_pairs),(),sub{
+        my $res =shift;
+        if($client->{debug}){
+            print "GET " . $API . "?" . join("&",@query_string_pairs),"\n";
+            print $res->as_string,"\n";
+        }
         my $reply;
         my $data = JSON->new->utf8->decode($res->content);
         return if $data->{code}=~/^4000[1-7]$/;
@@ -38,9 +36,10 @@ sub SmartReply{
         elsif($data->{code}== 200000){
             $reply = encode("utf8","$data->{text}\n$data->{url}");
         }
+        $reply  = "\@$from_nick " . $reply  if rand(100)>20;
+        $reply = truncate($reply);
         $client->reply_message($msg,$reply) if $reply;
-    }
-    else{return }
+    });
      
 }
 1;
