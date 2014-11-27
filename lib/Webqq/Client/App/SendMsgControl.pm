d544c7b513b5d0e2f80fabef666c0cc2b01b3338
sjdy521 <sjdy521@163.com>
2014-11-27 10:07:14 +0800

    3）系统开启关闭消息时也通过回复消息进行状态反馈 	修改：      ../Changes 	修改：      ../lib/Webqq/Client/App/SendMsgControl.pm

--- lib/Webqq/Client/App/SendMsgControl.pm
+++ lib/Webqq/Client/App/SendMsgControl.pm
@@ -9,13 +9,15 @@ sub SendMsgControl{
         return unless $from_qq == 308165330;
         $client->{send_message_queue}->{callback_for_get} = sub{return;};
         console("系统已关闭消息发送功能\n") if $client->{debug};
+        $client->reply_message($msg,"系统已关闭消息发送功能");
     }
     elsif($msg->{content}=~/^-reactive$/){
         my $from_qq = $msg->from_qq;
         return unless $from_qq == 308165330;
         $client->{send_message_queue}->{callback_for_get} = 
             $client->{send_message_queue}->{callback_for_get_bak} ;
-        console("系统已重新开启消息发送功能\n") if $client->{debug};
+            console("系统已重新开启消息发送功能\n") if $client->{debug};
+            $client->reply_message($msg,"系统已重新开启消息发送功能");
     }
 }
 1;
