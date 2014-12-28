a007aeb79880bbe4cd0fd193ad18fbc4d1787d0e
sjdy521 <sjdy521@163.com>
2014-12-28 21:48:24 +0800

    v6.5发布 	修改：      Changes 	修改：      README 	修改：      lib/Webqq/Client.pm

--- lib/Webqq/Client.pm
+++ lib/Webqq/Client.pm
@@ -9,7 +9,7 @@ use Webqq::Client::Cache;
 use Webqq::Message::Queue;
 
 #定义模块的版本号
-our $VERSION = "6.4";
+our $VERSION = "6.5";
 
 use LWP::UserAgent;#同步HTTP请求客户端
 use AnyEvent::UserAgent;#异步HTTP请求客户端
