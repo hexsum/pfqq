d33b96a7f7230c1753d43d3b47ea0c4fd69cfdaa
sjdy521 <sjdy521@163.com>
2014-12-31 13:29:29 +0800

    2014-12-31 Webqq::Client v6.7 1）完善客户端消息发送间隔控制，可以设置$client->{send_interval}，默认1s 	修改：      Changes 	修改：      README 	修改：      lib/Webqq/Client.pm

--- lib/Webqq/Client.pm
+++ lib/Webqq/Client.pm
@@ -9,7 +9,7 @@ use Webqq::Client::Cache;
 use Webqq::Message::Queue;
 
 #定义模块的版本号
-our $VERSION = "6.6";
+our $VERSION = "6.7";
 
 use LWP::UserAgent;#同步HTTP请求客户端
 use AnyEvent::UserAgent;#异步HTTP请求客户端
