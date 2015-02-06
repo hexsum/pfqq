cde2949cabe5207e30d01e5eec8c07762647d188
sjdy521 <sjdy521@163.com>
2015-02-06 10:07:33 +0800

    修复CpanRecentModule插件的bug 	修改：      ../lib/Webqq/Client/Plugin/CpanRecentModule.pm

--- lib/Webqq/Client/Plugin/CpanRecentModule.pm
+++ lib/Webqq/Client/Plugin/CpanRecentModule.pm
@@ -2,9 +2,12 @@ package Webqq::Client::Plugin::CpanRecentModule;
 use AE;
 use XML::Simple;
 use POSIX qw(mktime);
+use Storable qw(store retrieve);
 use Webqq::Client::Util qw(console);
+my %data;
 sub call{
     my $client = shift;
+    my $path = shift;
     $client->{watchers}{rand()} = AE::timer 600,600,sub{
         print "GET https://metacpan.org/feed/recent?f=n\n" if $client->{debug};
         $client->{asyn_ua}->get('https://metacpan.org/feed/recent?f=n',(),sub{
@@ -18,43 +21,47 @@ sub call{
                 console "[Webqq::Client::Plugin::CpanRecentModule]$@\n" if $client->{debug};
                 return;
             }
-            my @module;
-            my ($S2,$M2,$H2,$d2,$m2,$y2) = gmtime;
-            $y2 = $y2+1900;
-            $m2 = $m2+1;
-            map {$_=sprintf "%02d",$_} ($S2,$M2,$H2,$d2,$m2);
-            map {$_=sprintf "%04d",$_} ($y2);
-
-            for my $item (@{ $xml->{'rdf:RDF'}{item} } ){
-                my ($y1,$m1,$d1,$H1,$M1,$S1) = $item->{'dc:date'}=~/(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})/;
-                #my ($y1,$m1,$d1,$H1,$M1,$S1) = split /-|T|Z|:/,$item->{'dc:date'};
-                map {$_ = substr($_,0,1)} ($M1,$M2);
-                next if "$y2-$m2-$d2-$H2-$M2" ne "$y1-$m1-$d1-$H1-$M1";
-                my $link = $client->get_dwz($item->{'link'});
-                $link = $item->{'link'} unless defined $link;
-                push @module,{
-                    author      =>  $item->{'dc:creator'},
-                    name        =>  $item->{title},
-                    'link'      =>  $link,
-                    desc        =>  $item->{description},
-                };
-            }
-
-            if(@module){
-                my @msg;
-                for(@module){
-                    push @msg ,
-                        "模块：$_->{name}\n" . 
-                        "描述：$_->{desc}\n" .
-                        "链接: $_->{link}\n" 
-                    ;
+            unless(%data){
+                for my $item (@{ $xml->{'rdf:RDF'}{item} } ){
+                    $data{$item->{title}} = {
+                        author      =>  $item->{'dc:creator'},
+                        'link'      =>  $item->{'link'},
+                        desc        =>  $item->{'description'},
+                        date        =>  $item->{'dc:date'},
+                    };
                 }
-                my $msg = "Hi, CPAN有新模块发布:\n" . join("\n",@msg);
-                for my $g (@{ $client->{qq_database}{group} }){
-                    $client->send_group_message(to_uin=>$g->{ginfo}{gid},content=>$msg);
+            }
+            else{
+                my @module;
+                for my $item (@{ $xml->{'rdf:RDF'}{item} } ){
+                    next if exists $data{$item->{title}};
+                    my $link = $client->get_dwz($item->{'link'});
+                    $link = $item->{'link'} unless defined $link;
+                    push @module,{
+                        author      =>  $item->{'dc:creator'},
+                        name        =>  $item->{title},
+                        'link'      =>  $link,
+                        desc        =>  $item->{description},
+                    };
+                }
+            
+                if(@module){
+                    my @msg;
+                    for(@module){
+                        push @msg ,
+                            "模块：$_->{name}\n" . 
+                            "描述：$_->{desc}\n" .
+                            "链接: $_->{link}\n" 
+                        ;
+                    }
+                    my $msg = "Hi, CPAN有新模块发布:\n" . join("\n",@msg);
+                    for my $g (@{ $client->{qq_database}{group} }){
+                        $client->send_group_message(to_uin=>$g->{ginfo}{gid},content=>$msg);
+                    }
                 }
             }
         });
     };
 }
+
 1;
